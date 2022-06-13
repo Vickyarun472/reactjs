f836798a4fcae7697667a7e64cdcd3d8c312a860
Vickyarun472 <vigneshwaran.raja@abstract-tech.com>
2022-06-13 15:15:58 +0530

    elibrary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './assets/font-awesome/css/font-awesome.min.css';
+import Header from './Components/Header/Header';
+import Register from './Components/Register/Register';
+import Login from './Components/Login/Login';
+import Home from './Components/HomePage/Homepage';
+import About from './Components/About/About';
+import Rules from './Components/Rules/Rules';
+import Price from './Components/Price/Price';
+import Search from './Components/Search/Search';
+import AdminHeader from './Components/AdminHeader/AdminHeader';
+import AdminSearch from './Components/AdminSearch/AdminSearch';
+import AddBook from './Components/AddBook/Addbook';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <BrowserRouter>
+        <Routes>
+          <Route path='' element={<Register/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/home' element={<><Header/><Home/></>}/>
+          <Route path='/about' element={<><Header/><About/></>}/>
+          <Route path='/rules' element={<><Header/><Rules/></>}/>
+          <Route path='/price' element={<><Header/><Price/></>}/>
+          <Route path='/search' element={<><Header/><Search/></>}/>
+          <Route path='/adminHome' element={<><AdminHeader/><Home/></>}/>
+          <Route path='/adminAbout' element={<><AdminHeader/><About/></>}/>
+          <Route path='/adminRules' element={<><AdminHeader/><Rules/></>}/>
+          <Route path='/adminPrice' element={<><AdminHeader/><Price/></>}/>
+          <Route path='/adminSearch' element={<><AdminHeader/><AdminSearch/></>}/>
+          <Route path='/addBook' element={<AddBook/>}/>
+        </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
-
 export default App;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,8 @@
+import './Home.css';
+import Libraryimage from '../../Assests/1.jpg';
+function Home(){
+    return(<div>
+        <div className="classdiv">Welcome to Library Management System<img src={Libraryimage} className="image1"></img></div>
+    </div>);
+}
+export default Home;
